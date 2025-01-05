12d5105142be508492c90593e3398f66d7cf9fda
Trisna Utama <trisnautama@MacBook-Pro-Trisna.local>
2025-01-05 23:38:46 +0800

    feature: adding navbar and footer to user side

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -4,3 +4,6 @@ export const ROOT_URL = "http://localhost:8000/api";
 // colors
 export const primaryColor = "#2C71F6";
 export const hoverPrimaryColor = "#2c6ff6e0";
+export const backgroundColor = "#EBEBEB";
+export const backgroundDark = "#1A2130";
+export const strokes = "#E1E1E1";
