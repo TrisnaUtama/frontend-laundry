8d1a60a832b6280ae1f99763f413435ec415467f
Trisna Utama <trisnautama@MacBook-Pro-Trisna.local>
2025-01-23 23:52:25 +0800

    feat:merging staging into main

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,14 @@
 import type { MetadataRoute } from "next";
 
-export default async function robots() : Promise<MetadataRoute.Robots>{
-    const baseUrl = "https://laundry-hub.vercel.app/";
+export default async function robots(): Promise<MetadataRoute.Robots> {
+	const baseUrl = "https://laundry-hub.vercel.app/";
 
-    return {
-        rules: {
-            userAgent: "*",
-            allow: ["/", "/auth"],
-            disallow: []
-        },
-        sitemap: `${baseUrl}/sitemap.xml`
-    }
+	return {
+		rules: {
+			userAgent: "*",
+			allow: ["/", "/auth"],
+			disallow: [],
+		},
+		sitemap: `${baseUrl}/sitemap.xml`,
+	};
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,20 @@
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "https://laundry-hub.vercel.app/";
+	const baseUrl = "https://laundry-hub.vercel.app/";
 
-  const urls = ["auth/sign-in", "auth/sign-up" ]
-const optimizedUrl = urls.map((val) => {
-    return {
-        url: `${baseUrl}${val}`,
-        lastModified: new Date()
-    }
-    
-})
-  return [
-    {
-        url: baseUrl,
-        lastModified: new Date()
-    },
-    ...optimizedUrl
-  ]
-
+	const urls = ["auth/sign-in", "auth/sign-up"];
+	const optimizedUrl = urls.map((val) => {
+		return {
+			url: `${baseUrl}${val}`,
+			lastModified: new Date(),
+		};
+	});
+	return [
+		{
+			url: baseUrl,
+			lastModified: new Date(),
+		},
+		...optimizedUrl,
+	];
 }
