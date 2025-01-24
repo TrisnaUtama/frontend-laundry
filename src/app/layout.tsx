8679215b60dcc37998ef8658cfa0d6e99ad6086e
Trisna Utama <trisnautama@MacBook-Pro-Trisna.local>
2025-01-24 15:25:35 +0800

    feat:merging staging into main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,37 +4,37 @@ import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 
 const quicksand = Quicksand({
-	subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: {
-		default: "Laundry Hub",
-		// biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
-		template: `%s | Laundry Hub`,
-	},
-	openGraph: {
-		description: "create with love and neat",
-		images: "/landing-page.png",
-	},
-	// applicationName: "google-site-verification",
-	verification: {
-		google: "nriBV-HuufyRSMI3ohhaOVYR2alVHQkkOlDUEeJGnoI",
-	},
-	metadataBase: new URL("https://laundry-hub.vercel.app/"),
+  title: {
+    default: "Laundry Hub",
+    // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
+    template: `%s | Laundry Hub`,
+  },
+  openGraph: {
+    description: "create with love and neat",
+    images: [{ url: "/landing-page.png" }],
+  },
+  // applicationName: "google-site-verification",
+  verification: {
+    google: "nriBV-HuufyRSMI3ohhaOVYR2alVHQkkOlDUEeJGnoI",
+  },
+  metadataBase: new URL("https://laundry-hub.vercel.app/"),
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${quicksand.className} bg-[#EBEBEB] antialiased`}>
-				{children}
-				<Toaster />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${quicksand.className} bg-[#EBEBEB] antialiased`}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
 }
