866cfa1271cae99df5902d6f44206533bb4e0566
TrisnaUtama <trisnautama@MacBook-Pro-Trisna.local>
2025-01-24 15:47:20 +0800

    feat:merging staging into main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,43 +4,46 @@ import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 
 const quicksand = Quicksand({
-  subsets: ["latin"],
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: "Laundry Hub",
-    // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
-    template: `%s | Laundry Hub`,
-  },
-  icons: {
-    icon: "https://laundry-hub.vercel.app/landing-page.png",
-  },
-  openGraph: {
-    description: "create with love and neat",
-    images: [{ url: "https://laundry-hub.vercel.app/landing-page.png" }],
-  },
-  // applicationName: "google-site-verification",
-  verification: {
-    google: "nriBV-HuufyRSMI3ohhaOVYR2alVHQkkOlDUEeJGnoI",
-  },
-  metadataBase: new URL("https://laundry-hub.vercel.app/"),
+	title: {
+		default: "Laundry Hub",
+		// biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
+		template: `%s | Laundry Hub`,
+	},
+	icons: {
+		icon: "https://laundry-hub.vercel.app/landing-page.png",
+	},
+	openGraph: {
+		description: "create with love and neat",
+		images: [{ url: "https://laundry-hub.vercel.app/landing-page.png" }],
+	},
+	// applicationName: "google-site-verification",
+	verification: {
+		google: "nriBV-HuufyRSMI3ohhaOVYR2alVHQkkOlDUEeJGnoI",
+	},
+	metadataBase: new URL("https://laundry-hub.vercel.app/"),
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="https://laundry-hub.vercel.app/landing-page.png" />
-      </head>
-      <body className={`${quicksand.className} bg-[#EBEBEB] antialiased`}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link
+					rel="icon"
+					href="https://laundry-hub.vercel.app/landing-page.png"
+				/>
+			</head>
+			<body className={`${quicksand.className} bg-[#EBEBEB] antialiased`}>
+				{children}
+				<Toaster />
+			</body>
+		</html>
+	);
 }
