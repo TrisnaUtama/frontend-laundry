18d824d1e8c8ab0852f02fec2c6681ec46adb088
TrisnaUtama <trisnautama@MacBook-Pro-Trisna.local>
2025-01-24 15:47:14 +0800

    feat:merging staging into main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head> */}
+      <head>
+        <link rel="icon" href="https://laundry-hub.vercel.app/landing-page.png" />
+      </head>
       <body className={`${quicksand.className} bg-[#EBEBEB] antialiased`}>
         {children}
         <Toaster />
